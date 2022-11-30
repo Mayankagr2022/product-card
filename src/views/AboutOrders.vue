c77ae613f49717b394e25f3d01f290b83d6c1e38
Mayank Agarwal <arenmayank20@gmail.com>
2022-11-30 14:39:59 +0530

    Product-card

---
+++ src/views/AboutOrders.vue
@@ -0,0 +1,33 @@
+<template>
+    <main class="wrapper">
+        <h1>About Products</h1>
+        <div>
+            <h3>1. Raddishes</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cum harum laborum provident consectetur dignissimos inventore quod incidunt enim earum magnam temporibus, iste amet reprehenderit vero commodi culpa dicta eveniet!</p>
+        </div>
+        <div>
+            <h3>2. Artichokes</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, minus qui dolor veritatis reiciendis ullam necessitatibus sapiente quo officia dicta, voluptatem at placeat soluta eos, quis vitae ipsam. Similique, saepe</p>
+        </div>
+        <div>
+            <h3>3. Broccoli</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus itaque neque adipisci illum exercitationem et laudantium iusto, consectetur vero rem esse sit dolores voluptate perspiciatis saepe dolorem deserunt quam iste</p>
+        </div>
+        <div>
+            <h3>4. Cabbages</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum provident quis praesentium culpa quisquam dolorum enim accusantium, at cupiditate soluta cumque rerum a expedita. Dignissimos libero reiciendis unde nemo id.
+            </p>
+        </div>
+        <div>
+            <h3>5. Cherries</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem nemo esse aspernatur, temporibus veniam hic porro enim blanditiis! Corrupti quasi mollitia iure sunt maxime debitis ipsam impedit atque et.
+            </p>
+        </div>
+        <div>
+            <h3>6. Carrots</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, impedit rerum, illo possimus hic officia cupiditate veritatis est voluptas fuga, pariatur maxime repellendus quidem provident optio aliquam doloribus aut debitis!
+            </p>
+        </div>
+        
+    </main>
+</template>
